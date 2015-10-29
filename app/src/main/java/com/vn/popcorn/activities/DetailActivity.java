bb7138353e23340921b1d4f27b5742b05b21f821
vishnunarayanan <vishnunarayanan@qburst.com>
2015-10-29 15:02:49 +0530

    Add placeholder image to Detail Activity

--- app/src/main/java/com/vn/popcorn/activities/DetailActivity.java
+++ app/src/main/java/com/vn/popcorn/activities/DetailActivity.java
@@ -56,6 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             Picasso.with(this)
                     .load(posterUri)
                     .fit()
+                    .placeholder(R.drawable.placeholder)
                     .into((ImageView)findViewById(R.id.imageView));
         }
 
